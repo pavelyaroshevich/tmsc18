eb2a73554523adf54ef9158ac89663b78b00b28e
Lenovo <@pavelyaroshevich>
2022-10-25 14:54:01 +0300

    создали Test2 и Test3

---
+++ Lesson2/src/main/java/Test3.java
@@ -0,0 +1,9 @@
+public class Test3 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 1;
+        b = 2;
+        c = a + b;
+        System.out.println(c);
+    }
+}
