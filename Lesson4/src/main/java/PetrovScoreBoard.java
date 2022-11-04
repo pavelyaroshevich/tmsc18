3795d8c1527b1e96b410af97cd2cf31a7fdd794e
Lenovo <@pavelyaroshevich>
2022-11-04 14:24:36 +0300

    Переделал с часами, но мне не нравится то, что обрезается остаток. Возможно надо строить код через другой класс?

--- Lesson4/src/main/java/PetrovScoreBoard.java
+++ Lesson4/src/main/java/PetrovScoreBoard.java
@@ -13,24 +13,15 @@ public class PetrovScoreBoard {
     public static void main(String[] args) {
         int random = new Random().nextInt(28800);
         System.out.println("До конца рабочего дня осталось " + random + " секунд!");
-        if (random > 25200) {
-            System.out.println("До конца рабочего дня осталось 8 часов!");
-        } else if (random > 21600) {
-            System.out.println("До конца рабочего дня осталось 7 часов!");
-        } else if (random > 18000) {
-            System.out.println("До конца рабочего дня осталось 6 часов!");
-        } else if (random > 14400) {
-            System.out.println("До конца рабочего дня осталось 5 часов!");
-        } else if (random > 10800) {
-            System.out.println("До конца рабочего дня осталось 4 часа!");
-        } else if (random > 7200) {
-            System.out.println("До конца рабочего дня осталось 3 часа!");
-        } else if (random > 3600) {
-            System.out.println("До конца рабочего дня осталось 2 часа!");
-        } else if (random > 0) {
-            System.out.println("До конца рабочего дня остался 1 час!");
+        int hours = random / 3600;
+        if (hours > 4) {
+            System.out.println("До конца рабочего дня осталось " + hours + " часов");
+        } else if (hours == 1) {
+            System.out.println("До конца рабочего дня остался " + hours + " час");
+        } else if (hours > 0) {
+            System.out.println("До конца рабочего дня осталось " + hours + " часа");
         } else {
-            System.out.println("Рабочий день окончен!");
+            System.out.println("До конца рабочего дня осталось менее часа");
         }
     }
 }
