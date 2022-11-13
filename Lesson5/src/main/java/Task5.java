1d7705ee48116fb1ebebffa49dd77d8d2de693ce
Lenovo <@pavelyaroshevich>
2022-11-13 17:28:39 +0300

    5 задача, исправил косячок

--- Lesson5/src/main/java/Task5.java
+++ Lesson5/src/main/java/Task5.java
@@ -2,7 +2,7 @@ public class Task5 {
     public static void main(String[] args) {
         //        5) Напишите реализацию метода sum(int a, int b), вычисляющий a*b, не пользуясь операцией
         //        умножения, где a и b целые числа, вызовите метод sum в методе main и распечатайте на консоль.
-        System.out.println(sum(-3, 3));
+        System.out.println(sum(3, 3));
     }
 
     public static int sum(int a, int b) {
@@ -18,6 +18,10 @@ public class Task5 {
         } else if (a < 0 || b < 0) {
             for (int i = 0; i < Math.abs(b); i++) {
                 sum += Math.abs(a) * -1;
+            }
+        } else {
+            for (int i = 0; i < Math.abs(b); i++) {
+                sum += Math.abs(a);
             }
         }
         return sum;
