cc47f87c06dbf48c99ec2e533dd34314ef7494bc
Lenovo <@pavelyaroshevich>
2022-11-13 17:25:19 +0300

    5,7,4 задачи

--- Lesson5/src/main/java/Task7.java
+++ Lesson5/src/main/java/Task7.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Task7 {
     //        7) Создайте массив из всех нечётных чисел от 1 до 100, выведите его на экран в строку,
@@ -8,15 +9,13 @@ public class Task7 {
     }
 
     private static void reverseMass() {
-        int[] mass = new int[50];
+        Integer[] mass = new Integer[50];
         for (int i = 0; i < mass.length; i++) {
             mass[i] = 2 * i + 1;
         }
         System.out.println(Arrays.toString(mass));
         System.out.println("Наоборот");
-        for (int j = mass.length - 1; j >= 0; j--) {
-            mass[j] = 2 * mass.length - 2 * j - 1;
-        }
+        Arrays.sort(mass, Collections.reverseOrder());
         System.out.println(Arrays.toString(mass));
     }
 }
