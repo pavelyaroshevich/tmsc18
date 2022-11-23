91b30b4ae9b80a269b8a7114e7f868b030483545
Lenovo <@pavelyaroshevich>
2022-11-23 10:26:59 +0300

    1 задача 8го урока

--- Lesson7/src/main/java/by/tms/service/Main.java
+++ Lesson7/src/main/java/by/tms/service/Main.java
@@ -4,7 +4,7 @@ import by.tms.model.Computer;
 
 public class Main {
     public static void main(String[] args) {
-        Computer computer = new Computer( 2.5, 16, 512, 4);
+        Computer computer = new Computer(2.5, 16, 512, 4);
         System.out.println(computer);
         computer.onOrOff();
     }
