06b4c1665914323c09aabbdfe192775612b190a9
Lenovo <@pavelyaroshevich>
2023-01-10 16:02:51 +0300

    Домашка с Магазином. Первая часть. Не используя классы компаратора.

--- Lesson17/src/main/java/by/tms/store/services/Main.java
+++ Lesson17/src/main/java/by/tms/store/services/Main.java
@@ -2,31 +2,35 @@ package by.tms.store.services;
 
 import by.tms.store.models.Product;
 import by.tms.store.models.Shop;
-import by.tms.store.utils.ProductIdComparator;
-import by.tms.store.utils.ProductPriceComparator;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
+        Comparator<Product> comparatorById = Comparator.comparing(Product::getId);
+        Comparator<Product> comparatorByPrice = Comparator.comparing(Product::getPrice);
+        Comparator<Product> comparatorReversedById = Comparator.comparing(Product::getId).reversed();
         Shop shop = new Shop();
+
         Product milk = new Product(1L, "Brest-Litovsk", 2);
         Product cheese = new Product(3L, "Brest-Litovsk", 10);
         Product bear = new Product(2L, "Baltika", 3);
         Product juice = new Product(4L, "Dobri", 5);
+
         shop.addProduct(milk);
         shop.addProduct(cheese);
         shop.addProduct(bear);
         shop.addProduct(juice);
 
         System.out.println(shop.getListOfProducts()); // по порядку добавления
-        Set<Product> products = new TreeSet<>(new ProductPriceComparator());
+        Set<Product> products = new TreeSet<>(comparatorByPrice);
         products.addAll(shop.getAllProducts());
         System.out.println(products);
 
         shop.deleteProduct(3L);
-        TreeSet<Product> products1 = new TreeSet<>(new ProductIdComparator());
+        TreeSet<Product> products1 = new TreeSet<>(comparatorReversedById);
         products1.addAll(shop.getAllProducts());
         System.out.println(products1);
 
