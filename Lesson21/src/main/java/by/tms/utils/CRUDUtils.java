9fff27dcf646a066639dcb0e539d758a94578a09
Lenovo <@pavelyaroshevich>
2023-01-23 14:02:49 +0300

    Обновил с городом

--- Lesson21/src/main/java/by/tms/utils/CRUDUtils.java
+++ Lesson21/src/main/java/by/tms/utils/CRUDUtils.java
@@ -1,5 +1,6 @@
 package by.tms.utils;
 
+import by.tms.models.City;
 import by.tms.models.Student;
 
 import java.sql.Connection;
@@ -11,9 +12,6 @@ import java.util.List;
 
 public class CRUDUtils {
     private static final String GET_ALL_STUDENTS_QUERY = "select * from students left join  students_city sc on sc.city_id = students.id";
-    private static final String INSERT_STUDENT_QUERY = "INSERT INTO students(name, surname, course) VALUES(?, ?, ?);";
-    private static final String UPDATE_STUDENT_QUERY = "UPDATE students SET course = ? WHERE id = ?;";
-    private static final String DELETE_STUDENT_QUERY = "DELETE FROM students WHERE id = ?";
 
     public static List<Student> getAllStudents() {
         List<Student> students = new ArrayList<>();
@@ -26,8 +24,8 @@ public class CRUDUtils {
                 String name = rs.getString("name");
                 String surname = rs.getString("surname");
                 int course = rs.getInt("course");
-                String city = rs.getString(("city"));
-                students.add(new Student(id, name, surname, course, city));
+                String cityName = rs.getString("city");
+                students.add(new Student(id, name, surname, course, new City(cityName)));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
